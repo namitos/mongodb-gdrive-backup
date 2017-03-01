86879805839d43be8699f0979d43ff9d948fd179
Artem <namit@yandex.ru>
2017-03-01 19:40:49 +0300

    refactor

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
 COPY . /usr/src/app
-RUN apt-get update \
-	&& apt-get install -y tar \
+RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 \
+  && echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list \
+  && apt-get update \
+	&& apt-get install -y tar mongodb-org-tools \
 	&& npm install --unsafe-perm
 
 CMD [ "node", "app" ]
--- app.js
+++ app.js
@@ -1,90 +1,87 @@
-let fs = require('fs');
+const fs = require('fs');
 
-let google = require('googleapis');
-let drive = google.drive('v3');
-let moment = require('moment');
+const google = require('googleapis');
+const drive = google.drive('v3');
+const moment = require('moment');
 
-let conf = require(process.env.conf ? process.env.conf : './conf');
+const conf = require(process.env.conf ? process.env.conf : './conf');
 
 let jwtClient = new google.auth.JWT(conf.googleServiceAccount.client_email, null, conf.googleServiceAccount.private_key, [
-	'https://www.googleapis.com/auth/drive'
+  'https://www.googleapis.com/auth/drive'
 ], null);
 google.options({auth: jwtClient});
 
 
 function sendBackup(emailToShare, name, buffer) {
-	return new Promise((resolve, reject) => {
-		jwtClient.authorize((err, tokens) => {
-			if (err) {
-				console.log('authorize', err);
-				reject(err);
-			} else {
-				drive.files.create({
-					resource: {
-						name: name,
-						mimeType: 'application/tar+gzip'
-					},
-					media: {
-						mimeType: 'application/tar+gzip',
-						body: buffer
-					}
-				}, function (err, file) {
-					if (err) {
-						reject(err);
-					} else {
-						drive.permissions.create({
-							fileId: file.id,
-							sendNotificationEmail: false,
-							resource: {
-								role: 'reader',
-								type: 'user',
-								emailAddress: emailToShare,
-								value: emailToShare
-							}
-						}, (err, res) => {
-							err ? reject(err) : resolve(res);
-						});
-					}
-				});
-			}
-		});
-	});
+  return new Promise((resolve, reject) => {
+    jwtClient.authorize((err, tokens) => {
+      if (err) {
+        console.log('authorize', err);
+        reject(err);
+      } else {
+        drive.files.create({
+          resource: {
+            name: name,
+            mimeType: 'application/tar+gzip'
+          },
+          media: {
+            mimeType: 'application/tar+gzip',
+            body: buffer
+          }
+        }, function (err, file) {
+          if (err) {
+            reject(err);
+          } else {
+            drive.permissions.create({
+              fileId: file.id,
+              sendNotificationEmail: false,
+              resource: {
+                role: 'reader',
+                type: 'user',
+                emailAddress: emailToShare,
+                value: emailToShare
+              }
+            }, (err, res) => {
+              err ? reject(err) : resolve(res);
+            });
+          }
+        });
+      }
+    });
+  });
 }
 
 function exec(command, options) {
-	return new Promise((resolve, reject) => {
-		require('child_process').exec(command, options, (error, stdout, stderr) => {
-			error ? reject(error) : resolve(stdout);
-		})
-	});
+  return new Promise((resolve, reject) => {
+    require('child_process').exec(command, options, (error, stdout, stderr) => {
+      error ? reject(error) : resolve(stdout);
+    })
+  });
 }
 
 function dump() {
-	let command = process.env.docker ? "./bin/mongodump" : "mongodump";
-	exec(command + ' --host ' + (conf.mongo.host ? conf.mongo.host : '127.0.0.1') + ':' + (conf.mongo.port ? conf.mongo.port : 27017)).then(() => {
-		return exec('tar -cvzf dump.tar.gz dump && rm -rf dump');
-	}).then(() => {
-		return sendBackup(conf.backup.shareTo, (conf.backup.fileName ? conf.backup.fileName : 'Backup') + ' ' + new Date().toString() + '.tar.gz', fs.createReadStream('dump.tar.gz'));
-	}).then(() => {
-		console.log('backup success');
-		return exec('rm dump.tar.gz');
-	}).then(() => {
-		//cleaning old files
-		drive.files.list({
-			pageSize: 1000,
-			q: "modifiedTime < '" + moment().subtract(conf.backup.deleteTimeout ? conf.backup.deleteTimeout : 14, 'days').format("YYYY-MM-DDTHH:mm:ss") + "'"
-		}, (err, resp) => {
-			resp.files.forEach((file) => {
-				drive.files.delete({
-					fileId: file.id
-				}, (err, result) => {
-					console.log(err, result)
-				});
-			});
-		});
-	}).catch((err) => {
-		console.error('error:', err);
-	});
+  exec(`mongodump --host ${conf.mongo.host ? conf.mongo.host : '127.0.0.1'}:${conf.mongo.port ? conf.mongo.port : '27017'} && tar -cvzf dump.tar.gz dump && rm -rf dump`)
+    .then(() => sendBackup(conf.backup.shareTo, (conf.backup.fileName ? conf.backup.fileName : 'Backup') + ' ' + new Date().toString() + '.tar.gz', fs.createReadStream('dump.tar.gz')))
+    .then(() => exec('rm dump.tar.gz'))
+    .then(() => {
+      console.log('backup success');
+      //cleaning old files
+      drive.files.list({
+        pageSize: 1000,
+        q: "modifiedTime < '" + moment().subtract(conf.backup.deleteTimeout ? conf.backup.deleteTimeout : 14, 'days').format("YYYY-MM-DDTHH:mm:ss") + "'"
+      }, (err, resp) => {
+        resp.files.forEach((file) => {
+          drive.files.delete({
+            fileId: file.id
+          }, (err, result) => {
+            console.log(err, result)
+          });
+        });
+      });
+    })
+    .catch((err) => {
+      console.error('error:', err);
+    });
 }
 
 dump();
