a00642e5044ad56ba5d1293e114858df00ff5c07
Artem <namitos@users.noreply.github.com>
2017-05-17 11:11:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -59,28 +59,39 @@ function exec(command, options) {
   });
 }
 
+function clean() {
+  //cleaning old files
+  drive.files.list({
+    pageSize: 1000,
+    q: "modifiedTime < '" + moment().subtract(conf.backup.deleteTimeout ? conf.backup.deleteTimeout : 14, 'days').format("YYYY-MM-DDTHH:mm:ss") + "'"
+  }, (err, resp) => {
+    resp.files.forEach((file) => {
+      drive.files.delete({
+        fileId: file.id
+      }, (err, result) => {
+        console.log(err, result)
+      });
+    });
+  });
+}
+
 function dump() {
   exec(`mongodump --host ${conf.mongo.host ? conf.mongo.host : '127.0.0.1'}:${conf.mongo.port ? conf.mongo.port : '27017'} && tar -cvzf dump.tar.gz dump && rm -rf dump`)
     .then(() => sendBackup(conf.backup.shareTo, (conf.backup.fileName ? conf.backup.fileName : 'Backup') + ' ' + new Date().toString() + '.tar.gz', fs.createReadStream('dump.tar.gz')))
     .then(() => exec('rm dump.tar.gz'))
     .then(() => {
       console.log('backup success');
-      //cleaning old files
-      drive.files.list({
-        pageSize: 1000,
-        q: "modifiedTime < '" + moment().subtract(conf.backup.deleteTimeout ? conf.backup.deleteTimeout : 14, 'days').format("YYYY-MM-DDTHH:mm:ss") + "'"
-      }, (err, resp) => {
-        resp.files.forEach((file) => {
-          drive.files.delete({
-            fileId: file.id
-          }, (err, result) => {
-            console.log(err, result)
-          });
-        });
-      });
+      clean();
     })
     .catch((err) => {
       console.error('error:', err);
+      try {
+        if(err.errors[0].reason === 'storageQuotaExceeded') {
+          clean();
+        }
+      } catch (e) {
+        console.error('error:', err);
+      }
     });
 }
 
